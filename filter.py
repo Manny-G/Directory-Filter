a7080b4bbb19b4609652b079f31afd6880085b61
Manny-g <mannyguzman88@gmail.com>
2018-05-19 13:31:18 -0500

    Updates to make the searching targetable from other directories, overrides same name directories.

--- filter.py
+++ filter.py
@@ -1,29 +1,40 @@
 #! python3
-# filter.py [dir_name] [file_ext] - Search a directory recursive for files with
+# filter.py [dir_name] [file_ext] - Search a directory recursively for files with
 #									repeat names and separate them into different
 #									folders. Enter desired directory to filter
 #									and file extension as arguments in the
 #									command line.
 
-import glob, os, shutil, sys
+import glob, os, shutil, sys, time
 
 dir_name = sys.argv[1]
 file_ext = sys.argv[2]
 
-orig_dir = "originals_" + file_ext
-rep_dir = "repeats_" + file_ext
-generic_file = "./**/*." + file_ext
+orig_dir = 'originals_' + file_ext
+rep_dir = 'repeats_' + file_ext
+generic_file = './**/*.' + file_ext
+
+if os.path.isdir(orig_dir):
+	shutil.rmtree(orig_dir)
+	
+if os.path.isdir(rep_dir):
+	shutil.rmtree(rep_dir)
+	
+time.sleep(5)
 
 os.makedirs(orig_dir)
 os.makedirs(rep_dir)
-os.chdir(dir_name)
+
+if not(dir_name == '.' or dir_name == './' or dir_name == '.\\'):
+	starting_dir = os.getcwd()
+	os.chdir(dir_name)
 
 # initialize empty dictionary and current working directory
 filenames_dict = {}
 curr_dir = os.getcwd()
 
-print("\nCurrently in", curr_dir, "\n")
-print("List of all recursive files:")
+print('\nCurrently in', curr_dir, '\n')
+print('List of all recursive files:')
 
 # recurse through all files in the directory
 for filename in glob.iglob(generic_file, recursive = True):
@@ -33,29 +44,25 @@ for filename in glob.iglob(generic_file, recursive = True):
 	relative_path = os.path.dirname(filename)
 	
 	# check if this file has already been seen, then store the relative path
-	filenames_dict.setdefault( base_name, [] )
-	filenames_dict[base_name].append( relative_path )
+	filenames_dict.setdefault(base_name, [])
+	filenames_dict[base_name].append(relative_path)
 	
 	print(filename)
 
-os.chdir("../")
-
+if not(dir_name == '.' or dir_name == './' or dir_name == '.\\'):
+	os.chdir(starting_dir)
 
 for filename in filenames_dict:
 	# keep track of how many times we've seen this file
 	counter = 0
-	print( "\nFile '{}' was found in: ".format(filename) )
+	print('\nFile ', str(filename),' was found in: ')
 	
 	for filepath in filenames_dict[filename]:
 		counter += 1
-		print( "Encounter number {} at path {}".format(counter, filepath) )
+		print('Encounter number ', str(counter), ' at path ', str(filepath))
 		
-		if counter == 1:
-			source_dir = "./" + dir_name + filepath + "/" + filename
-			dest_dir = "./" + orig_dir + "/" + filename
-			shutil.move( source_dir, dest_dir )
-			
-		else:
-			source_dir = "./" + dir_name + filepath + "/" + filename
-			dest_dir = "./" + rep_dir + "/" + filename
-			shutil.move( source_dir, dest_dir )
+		source_dir = './' + dir_name + filepath + '/' + filename
+		dest_dir = './' + orig_dir + '/' + filename if counter == 1 else './' + rep_dir + '/' + filename
+		shutil.copy(source_dir, dest_dir)
+		
+
